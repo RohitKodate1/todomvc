f9f34858f493e0bf0c21098579cecf666f92c2c9
Rohit Kodate <rohit@aedo.ai>
2022-07-28 16:57:15 -0400

    trigger workflow

--- cypress/integration/Datahub_Purpose_Fail.js
+++ cypress/integration/Datahub_Purpose_Fail.js
@@ -1,6 +1,6 @@
 it("Datahub Visit", ()=>  {
     cy.visit("https://aedo-data-foundation-fd-stg.azurefd.net/")
-    cy.contains('SIGN OUT')
+    cy.contains('SIGN IN')
     cy.wait(3000)
     cy.wait(3000)
     cy.wait(3000)
