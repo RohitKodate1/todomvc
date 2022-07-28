b8c504baa2ebb6ed35bd8b80391ce3ab36b7987d
Rohit Kodate <rohit@aedo.ai>
2022-07-28 16:00:50 -0400

    trigger workflow

--- cypress/integration/Datahub_visit.js
+++ cypress/integration/Datahub_visit.js
@@ -1,5 +1,9 @@
 it("Datahub Visit", ()=>  {
     cy.visit("https://aedo-data-foundation-fd-stg.azurefd.net/")
     cy.contains('SIGN IN')
+    cy.wait(3000)
+    cy.wait(3000)
+    cy.wait(3000)
+    cy.wait(3000)
 
 });
